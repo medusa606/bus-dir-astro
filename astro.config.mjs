4f4af13a30449e9f82cbc3ca0e0a284453753d29
Greg Chance <greg.chance@gmail.com>
2026-05-15 15:50:49 +0100

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-    site: 'https://meridian-directory.pages.dev',
+    site: 'https://curatedlocale.uk',
     output: 'static',
     adapter: cloudflare(),
     trailingSlash: 'never',
