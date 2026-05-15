508e8b3c5504afe7dec5259af97e3d48abc2eab9
Greg Chance <greg.chance@gmail.com>
2026-05-15 19:37:23 +0100

    astro static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
     site: 'https://curatedlocale.uk',
     output: 'static',
-    adapter: cloudflare(),
     trailingSlash: 'never',
     prefetch: {
         defaultStrategy: 'hover'
